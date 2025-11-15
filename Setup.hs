496c94e8a3637a89418635e5cb7e03dd8707da75
Guybrush Threepwood <guybrush@live.ru>
2025-11-15 14:46:58 +0500

    Setup.hs experiments

--- Setup.hs
+++ Setup.hs
@@ -1,23 +1,22 @@
 {-# LANGUAGE OverloadedStrings #-}
-import Distribution.Simple
-import Distribution.Simple.Setup (BuildFlags(..))
-import Distribution.Simple.LocalBuildInfo
+
 import Distribution.PackageDescription
-import System.Process (callCommand, system)
-import System.Directory (doesFileExist, createDirectoryIfMissing, doesDirectoryExist)
-import System.FilePath ((</>))
-import System.Exit (ExitCode(..))
+import Distribution.Simple
+import Distribution.Simple.LocalBuildInfo
+import Distribution.Simple.Setup (BuildFlags (..))
 
 main :: IO ()
-main = defaultMainWithHooks simpleUserHooks
-  { buildHook = buildHook'
-  }
+main =
+  defaultMainWithHooks
+    simpleUserHooks
+      { buildHook = buildHook'
+      }
 
 buildHook' :: PackageDescription -> LocalBuildInfo -> UserHooks -> BuildFlags -> IO ()
 buildHook' pkg_descr local_bld_info user_hooks bld_flags = do
   -- Run the data generation before building
   generateData
-  
+
   -- Then run the normal build
   buildHook simpleUserHooks pkg_descr local_bld_info user_hooks bld_flags
 
